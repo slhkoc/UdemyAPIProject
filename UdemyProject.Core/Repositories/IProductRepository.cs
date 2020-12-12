6ad7ca128df4d133e1d157b1e43f38b067316776
salih <salihkoc3469@gmail.com>
2020-12-12 19:13:42 +0300

    Core Katmanı - IProductRepository Generic Interface Oluşturulması

---
+++ UdemyProject.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Models;
+
+namespace UdemyProject.Core.Repositories
+{
+    public interface IProductRepository:IRepository<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+
+
+    }
+}
