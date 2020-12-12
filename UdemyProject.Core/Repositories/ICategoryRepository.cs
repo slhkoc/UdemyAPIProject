580bef61ea149970c4b3d9b6b9a9c779c5bf195f
salih <salihkoc3469@gmail.com>
2020-12-12 19:09:12 +0300

    Core Katmanı - ICategoryRepository Generic Interface Oluşturulması

---
+++ UdemyProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Models;
+
+namespace UdemyProject.Core.Repositories
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+    }
+}
