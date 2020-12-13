c3aa37c4924df3be44e34332163287cc925eb420
salih <salihkoc3469@gmail.com>
2020-12-13 15:20:09 +0300

    Core Katmanı - ICategoryService İnterface'nin eklenmesi

---
+++ UdemyProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Models;
+
+namespace UdemyProject.Core.Services
+{
+    public interface ICategoryService:IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+        //Category özlü methodlarınız varsa burada tanımlayabilirsiniz.
+    }
+}
