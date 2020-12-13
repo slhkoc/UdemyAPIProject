001167628ae6bf9a8bdd0e4dd0825db93460b915
salih <salihkoc3469@gmail.com>
2020-12-13 16:21:16 +0300

    Data Katmanı - DbContext Sınıfının Oluşturulması ve EntityFramework Packages'lerinin yüklenmesi

---
+++ UdemyProject.Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyProject.Core.Models;
+
+namespace UdemyProject.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //Veritabanında tablolar oluşturulmadan önce çalışacak method
+        {
+           
+        }
+    }
+}
