27a6699ce650ac3a1973d144876cdd6fa92c33d1
salih <salihkoc3469@gmail.com>
2020-12-13 15:12:57 +0300

    Core Katmanı - IProductService interface'nin oluşturulması

---
+++ UdemyProject.Core/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Models;
+
+namespace UdemyProject.Core.Services
+{
+    public interface IProductService:IService<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+        //bool ControlInnerBarcode(Product product);
+
+    }
+}
