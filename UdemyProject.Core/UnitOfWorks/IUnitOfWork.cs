4e473b9b5a3a04833ce3c23e61a5b740f3d6db0c
salih <salihkoc3469@gmail.com>
2020-12-13 16:06:30 +0300

    Core Katmanı - IUnitOfWork Interface'nin oluşturulması

---
+++ UdemyProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Repositories;
+
+namespace UdemyProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
