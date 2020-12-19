02bb0fef19c8b6d7f73f7e0877d031437608685c
salih <salihkoc3469@gmail.com>
2020-12-19 17:09:10 +0300

    Data Katmanı - UnitOfWork.cs class'ının oluşturulması

---
+++ UdemyProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Repositories;
+using UdemyProject.Core.UnitOfWorks;
+using UdemyProject.Data.Repositories;
+
+namespace UdemyProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository=_categoryRepository ?? new CategoryRepository(_context);
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
