3e5df37f3705028dca14976aa5c696a751de8ea3
salih <salihkoc3469@gmail.com>
2020-12-19 16:19:41 +0300

    Data Katmanı - CategorySeed.cs Dosyasının oluşturulması

--- UdemyProject.Data/AppDbContext.cs
+++ UdemyProject.Data/AppDbContext.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Text;
 using UdemyProject.Core.Models;
 using UdemyProject.Data.Configurations;
+using UdemyProject.Data.Seeds;
 
 namespace UdemyProject.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
@@ -21,6 +22,9 @@ namespace UdemyProject.Data
         {
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1, 2 }));
+            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1, 2 }));
         }
     }
 }
