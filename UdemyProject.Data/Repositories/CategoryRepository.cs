55e10464eecc37519671c6ac6963501640243465
salih <salihkoc3469@gmail.com>
2020-12-19 17:00:57 +0300

    Data Katmanı - CategoryRepository.cs class'ının oluşturulması

---
+++ UdemyProject.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Models;
+using UdemyProject.Core.Repositories;
+
+namespace UdemyProject.Data.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public CategoryRepository(DbContext context) : base(context)
+        {
+
+        }
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            return await _appDbContext.Categories.Include(x => x.Products).SingleOrDefaultAsync(x => x.Id == categoryId);
+        }
+    }
+}
