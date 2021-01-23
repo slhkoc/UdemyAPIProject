ff31b95d91fbba8838696c42ce1a9715c1a5725f
salih <salihkoc3469@gmail.com>
2021-01-23 15:15:27 +0300

    Asp.Net Core MVC Web Projesi- Automapper kütüphanesinin yüklenmesi ve DTO sınıflarının oluşturulması

---
+++ UdemyProject.Web/DTOs/CategoryWithProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyProject.Web.DTOs;
+
+namespace UdemyProject.Web.DTOs
+{
+    public class CategoryWithProductDto:CategoryDto
+    {
+        public IEnumerable<ProductDto> Products { get; set; }
+
+    }
+}
