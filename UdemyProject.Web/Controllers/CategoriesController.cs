6a27bf9b1c28319f8cd955c494ddff977400588d
salih <salihkoc3469@gmail.com>
2021-01-23 15:21:04 +0300

    Asp.Net Core MVC Web Projesi - Categories Controllerının oluşturulması

---
+++ UdemyProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyProject.Core.Services;
+
+namespace UdemyProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public CategoriesController(ICategoryService categoryService,IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
